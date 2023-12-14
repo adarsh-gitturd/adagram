39f7f1fd1192127c41a1540ab9f81cf0a9846b5d
Adarsh Wilson <adarshwilsona@gmail.com>
2023-12-14 12:12:34 +0530

    fixed that retarded bug after wasting just about 3hrs

--- adagram/settings.py
+++ adagram/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "registration" / "templates"
+            #BASE_DIR / "registration" / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- registration/urls.py
+++ registration/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registration/', views.index)   
+    path("", views.index, name="t")   
 ]
