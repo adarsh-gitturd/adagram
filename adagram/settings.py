e7ac8c4d2c0354412d4408626242657c67aad121
Adarsh Wilson <adarshwilsona@gmail.com>
2023-12-14 11:27:14 +0530

    more

--- adagram/settings.py
+++ adagram/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'adagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': BASE_DIR / "challenges" / "templates",
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
